711ab7b962f43adde8f042b91beb9fdb88c6b664
dangkhuong14 <f13file31@gmail.com>
2023-01-17 19:58:05 +0700

    Add basic style set up to app component

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import {React, useState} from 'react'
+import { Text, View, ScrollView, Image, TextInput, StyleSheet } from 'react-native'
+import styles from './App.styles'
 
-const App = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'pink'}}>
-    <Text style={{ color: 'yellow', fontWeight: 'bold' }}>
-      Hello World:D
-      {/* Children Text will inherit style of the Text parents components */}
-      <Text style={{ color: 'green' }}>
-        How are you?
+const App = () =>{
+  const [status, setStatus]= useState('ok')
+  return (
+    <View style={styles.root}>
+      {/* <Text style={[styles.text, status == 'ok' ? styles.green : styles.red]}> */}
+      <Text style={[styles.text, {color: status == 'ok' ? 'green' : 'red' }]}>
+        {status === 'ok' ? 'Everything is Okay!' : 'Something went wrong!'}
       </Text>
-    </Text>
-  </View>
-)
+      <Image
+        source={{ uri: 'https://www.invert.vn/media/uploads/uploads/2022/12/03191809-2.jpeg' }}
+        style={styles.image}
+        resizeMode='cover'
+      />
+      <TextInput placeholder="Hi there, you can edit this line!" />
+    </View>
+  )
+} 
 
 export default App
---
+++ App.styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'pink'
+    },
+
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    image: {
+        backgroundColor: 'red',
+        height: 200,
+        width: 200
+    },
+
+    darkBackgroundColor:{
+        backgroundColor: 'gray'
+    },
+
+    red:{
+        color: 'red'
+    },
+
+    green:{
+        color: 'green'
+    }
+})
+
+export default styles
