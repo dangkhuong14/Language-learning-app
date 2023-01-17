6f3783bb0a89a1bc4ea33b595b8cc1e0b8075bb3
dangkhuong14 <f13file31@gmail.com>
2023-01-17 18:26:58 +0700

    Test App component with inherited style

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import { Text, View } from 'react-native'
 
 const App = () => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text>Hello World:D</Text>
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'pink'}}>
+    <Text style={{ color: 'yellow', fontWeight: 'bold' }}>
+      Hello World:D
+      {/* Children Text will inherit style of the Text parents components */}
+      <Text style={{ color: 'green' }}>
+        How are you?
+      </Text>
+    </Text>
   </View>
 )
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Hello World:D</Text>
-//       </View>
-//     )
-//   }
-// }
 
 export default App
