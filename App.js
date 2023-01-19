7871cdd1674f588255abf9672e132357b964fd1b
dangkhuong14 <f13file31@gmail.com>
2023-01-19 11:10:59 +0700

    test the Flex layout

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import {React, useState} from 'react'
+import React, {useState } from 'react'
 import { Text, View, ScrollView, Image, TextInput, StyleSheet } from 'react-native'
 import styles from './App.styles'
 
-const App = () =>{
-  const [status, setStatus]= useState('ok')
+const App = () => {
+  const [status, setStatus] = useState('ok')
+  console.log('Hello world');
   return (
     <View style={styles.root}>
-      {/* <Text style={[styles.text, status == 'ok' ? styles.green : styles.red]}> */}
-      <Text style={[styles.text, {color: status == 'ok' ? 'green' : 'red' }]}>
-        {status === 'ok' ? 'Everything is Okay!' : 'Something went wrong!'}
-      </Text>
-      <Image
-        source={{ uri: 'https://www.invert.vn/media/uploads/uploads/2022/12/03191809-2.jpeg' }}
-        style={styles.image}
-        resizeMode='cover'
-      />
-      <TextInput placeholder="Hi there, you can edit this line!" />
+      <View style={styles.item1}><Text style={styles.text}>1</Text></View>
+      <View style={styles.item2}><Text style={styles.text}>2</Text></View>
+      <View style={styles.item3}><Text style={styles.text}>3</Text></View>
+      {/*<View style={styles.item4}><Text style={styles.text}>4</Text></View>*/}
+      {/*<View style={styles.item5}><Text style={styles.text}>5</Text></View>*/}
+      {/*<View style={styles.item6}><Text style={styles.text}>6</Text></View>*/}
     </View>
   )
-} 
+}
 
 export default App
--- App.styles.js
+++ App.styles.js
@@ -2,34 +2,71 @@ import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
     root: {
+        flexDirection: 'row',
+        width: 200,
+
         flex: 1,
+        flexWrap: 'wrap-reverse',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'pink'
+        backgroundColor: 'pink',
     },
 
     text: {
+        fontSize: 20,
         color: 'white',
-        fontWeight: 'bold',
-        fontSize: 30
-    },
-    image: {
-        backgroundColor: 'red',
-        height: 200,
-        width: 200
     },
 
-    darkBackgroundColor:{
-        backgroundColor: 'gray'
+    item1: {
+
+        flexBasis: '70%',
+        height: 700,
+        borderWidth: 2,
+        boderRadious: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#2a80b9',
     },
 
-    red:{
-        color: 'red'
+    item2: {
+        height: 300,
+        flexBasis: '40%',
+        borderWidth: 2,
+        boderRadious: 20,
+        backgroundColor: '#8f44ad',
     },
 
-    green:{
-        color: 'green'
-    }
+    item3: {
+        alignSelf: 'flex-start',
+        flexBasis: '40%',
+        height: 400,
+        borderWidth: 2,
+        boderRadious: 20,
+        alignItems: 'center',
+
+        backgroundColor: '#16a086',
+    },
+
+    item4: {
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#f1c40f',
+    },
+
+    item5: {
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#e77e23',
+    },
+    item6: {
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#c1392b',
+    },
+
 })
 
 export default styles
